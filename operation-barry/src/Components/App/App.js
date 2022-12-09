b31ebb071373294ccf5ad086c4c31346cd1a2f65
november-golf <n.s.gray@live.co.uk>
2022-12-09 14:42:01 +0000

    useGet custom hook working. API call displays on page.

---
+++ operation-barry/src/Components/App/App.js
@@ -0,0 +1,44 @@
+import LoginButton from "../Auth0/login";
+import LogoutButton from "../Auth0/logout";
+import Profile from "../Auth0/profile";
+import "./App.css";
+import CancelLaunchButton from "../CancelLaunchButton"
+import React from "react";
+import {useState} from "react";
+import { useGet } from "../../hooks/useGet/useGet";
+
+
+function App() {
+  const [userName, setUserName] = useState(false)
+
+  const {response, error} = useGet("https://operationbarrybackend.onrender.com/items")
+  console.log(response, error)
+
+
+  function handleClick(e){
+    e.window.open("https://media.tenor.com/v2n3rVxTeJQAAAAd/jeff-goldblum.gif")
+    console.log(e)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">Ooops!</header>
+      
+      <p>{JSON.stringify(response)}</p>
+      
+      
+      
+      {/* <Profile setUserName={setUserName}/>
+ 
+      <div className="button-container">
+        <LoginButton  />
+        <LogoutButton />
+      {userName && (
+        <CancelLaunchButton handleClick={handleClick}/>)}
+      </div> */}
+      
+      {/* </div> */}
+    </div>
+  );
+}
+export default App;
