eed6799504421b25f44cb23b9031ea5e2a5b6fcf
november-golf <n.s.gray@live.co.uk>
2022-12-09 12:22:44 +0000

    Change to test auto-deploy on front-end

--- operation-barry/src/App.js
+++ operation-barry/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A brief history of the internet.
         </a>
       </header>
     </div>
